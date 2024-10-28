476dafa552ee9644e871f195682d9a976c0ebd2d
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-10-28 15:30:39 -0500

    Update Ping.js

--- plugins/Ping.js
+++ plugins/Ping.js
@@ -29,7 +29,7 @@ let texto = `*🚀 𝐕𝐞𝐥𝐨𝐜𝐢𝐝𝐚𝐝*
 *💻 𝐒𝐞𝐫𝐯𝐢𝐝𝐨𝐫 / nodovip*
 • *Ram:* ${format(totalmem() - freemem())} / ${format(totalmem())}`.trim()
 
-conn.sendMessage(m.chat, { text: texto, contextInfo: { externalAdReply: { title: '', body: '*ELITE BOT GLOBAL*', thumbnailUrl: 'https://qu.ax/YQAhw.jpg', sourceUrl: '', mediaType: 1, renderLargerThumbnail: true }}})
+conn.sendMessage(m.chat, { text: texto, contextInfo: { externalAdReply: { title: '', body: '𝙀𝙡𝙞𝙩𝙚𝘽𝙤𝙩𝙂𝙡𝙤𝙗𝙖𝙡', thumbnailUrl: 'https://qu.ax/YQAhw.jpg', sourceUrl: '', mediaType: 1, renderLargerThumbnail: true }}})
 
 }
 handler.help = ['ping']
