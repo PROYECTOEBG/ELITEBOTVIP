f3ec9f9a41a1e986bcf6389f0d40881e21b34e2c
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-10-28 16:39:55 -0500

    Update info-sc.js

--- plugins/Propietario/info-sc.js
+++ plugins/Propietario/info-sc.js
@@ -31,5 +31,5 @@ function clockString(ms) {
   const h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24;
   const m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
   const s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
-  return [`\n│ ❖ ` + d, ' Día(s)* ', `\n│ ❖ ` + h, ' Hora(s)* ', `\n│ ❖ ` + m, ' Minuto(s)* ', `\n│ ❖ ` + s, ' Segundo(s)* '].map((v) => v.toString().padStart(2, 0)).join('');
+  return [`\n│ ❖ ` + d, ' Día(s) ', `\n│ ❖ ` + h, ' Hora(s) ', `\n│ ❖ ` + m, ' Minuto(s) ', `\n│ ❖ ` + s, ' Segundo(s) '].map((v) => v.toString().padStart(2, 0)).join('');
 }
