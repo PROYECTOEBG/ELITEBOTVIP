a7f17769f8c0effc5d1f33e842d171a9897433db
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-10-28 16:31:16 -0500

    Update info-sc.js

--- plugins/Propietario/info-sc.js
+++ plugins/Propietario/info-sc.js
@@ -5,13 +5,13 @@ const handler = async (m, {conn}) => {
     const _uptime = process.uptime() * 1000;
     const uptime = clockString(_uptime);
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
-    const str = `╭─ 𝙀𝙇𝙄𝙏𝙀 𝘽𝙊𝙏 𝙂𝙇𝙊𝘽𝘼𝙇
+    const str = `╭━━━━━━━━━━━━━━✠
+┃  » 𝖡𝗎𝖾𝗇 𝖽í𝖺 𝗆𝗂 𝖼𝗋𝖾𝖺𝖽𝗈𝗋
+│    ${taguser}*
 │
-│ *➤ ʜᴏʟᴀ ${taguser}*
-│
-│ *=> ⏰ ᴛɪᴇᴍᴘᴏ ᴀᴄᴛɪᴠᴏ:* ${uptime}
+┃ 〽️ Estoy activo desde: ${uptime}
 │ 
-╰────────────────`.trim();
+╰━━━━━━━━━━━━━━✠`.trim();
     if (m.isGroup) {
       conn.sendMessage(m.chat, {text: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm, "containsAutoReply": true, "mediaType": 1, "thumbnail": pp, "mediaUrl": `https://www.atom.bio/theshadowbrokers-team`, "sourceUrl": `https://instagram.com/jeffomx15?igshid=OGQ5ZDc2ODk2ZA==`}}}, {quoted: m});
     } else {
@@ -31,5 +31,5 @@ function clockString(ms) {
   const h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24;
   const m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
   const s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
-  return [`\n│ *=> ⚡ ` + d, ' Día(s)* ', `\n│ *=> 💫 ` + h, ' Hora(s)* ', `\n│ *=> 💠 ` + m, ' Minuto(s)* ', `\n│ *=> ♦ ` + s, ' Segundo(s)* '].map((v) => v.toString().padStart(2, 0)).join('');
+  return [`\n│ ❖` + d, ' Día(s)* ', `\n│ ❖` + h, ' Hora(s)* ', `\n│ ❖` + m, ' Minuto(s)* ', `\n│ ❖` + s, ' Segundo(s)* '].map((v) => v.toString().padStart(2, 0)).join('');
 }
