8d09c3b61e76f512c1c9ff6a9929683d3c34f968
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-10-29 11:37:13 -0500

    Update idgrupo.js

--- plugins/Grupos/idgrupo.js
+++ plugins/Grupos/idgrupo.js
@@ -1,6 +1,6 @@
 var handler = async (m, {conn, groupMetadata }) => {
 
-conn.reply(m.chat, `${await groupMetadata.id}`, fkontak, fake, )
+conn.reply(m.chat, `𝗘𝗹𝗶𝘁𝗲𝗕𝗼𝘁𝗚𝗹𝗼𝗯𝗮𝗹 \n\n𝗘𝗹 𝗜𝗗 𝗱𝗲 𝗲𝘀𝘁𝗲 𝗴𝗿𝘂𝗽𝗼 𝗲𝘀 :\n${await groupMetadata.id}`, fkontak, )
 
 }
 handler.help = ['idgc']
