09e4563faca559cadd62841b62d194b8069c3dd1
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-10-31 10:30:12 -0500

    Update Play.js

--- plugins/Play.js
+++ plugins/Play.js
@@ -11,20 +11,19 @@ if (!play) return conn.reply(m.chat, `No se encontraron resultados`, m)
 
 let { title, thumbnail, ago, timestamp, views, videoId, url } = play;
 
-let txt = '
-01:27 ━━━━━⬤──── ${timestamp
+let txt = `01:27 ━━━━━⬤──── ${timestamp
 *⇄ㅤ   ◁   ㅤ  ❚❚ㅤ     ▷ㅤ   ↻*
 𝙀𝙡𝙞𝙩𝙚 𝘽𝙤𝙩 𝙂𝙡𝙤𝙗𝙖𝙡`;
 
 await conn.sendButton(m.chat, txt, author, thumbnail, [
 ['Audio', `${usedPrefix}ytmp3 ${url}`],
 ['Video', `${usedPrefix}ytmp4 ${url}`]
-], null, [['Canal', md]], m);
+], null, [['Hosting', md]], m);
 await m.react('✅')
 }
 
-handler.help = ['play', 'play2']
+handler.help = ['playy', 'play2']
 handler.tags = ['dl'];
-handler.command = ['playy', 'play2']
+handler.command = ['play', 'play2']
 
 export default handler;
