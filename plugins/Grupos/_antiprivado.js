0e3ac1399b27e006a22268a3507fd26e9f1c4adf
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-10-31 09:00:50 -0500

    Update _antiprivado.js

--- plugins/Grupos/_antiprivado.js
+++ plugins/Grupos/_antiprivado.js
@@ -1,25 +1,34 @@
-export async function before(m, {isOwner, isROwner}) {
-  if (m.isBaileys && m.fromMe) return !0;
-  if (m.isGroup) return !1;
-  if (!m.message) return !0;
-  if (m.text.includes("PIEDRA") || m.text.includes("PAPEL") || m.text.includes("TIJERA")) return !0;
-  let bot = global.db.data.settings[this.user.jid] || {};
+const comandos = /piedra|papel|tijera|estado|verificar|code|jadibot --code|--code|creadora|bottemporal|grupos|instalarbot|términos|bots|deletebot|eliminarsesion|serbot|verify|register|registrar|reg|reg1|nombre|name|nombre2|name2|edad|age|edad2|age2|genero|género|gender|identidad|pasatiempo|hobby|identify|finalizar|pas2|pas3|pas4|pas5|registroc|deletesesion|registror|jadibot/i
 
-  if (bot.antiPrivate && !isOwner && !isROwner) {
-    await m.reply(
-      `*Elite Bot Global es un bot mundial automatizado y tiene prohibido los mensajes a mi privado*
- | Seras bloqueado 
- 
-*Creador EBG:*
-👤 +593993370003
+export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }) {
+let prefixRegex = new RegExp('^[' + (opts['prefix'] || '‎z/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.,\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
 
-Nota:
-Llega a mi privado solo si tienes dinero para comprar el servicio.
-Cero sapos y cero niños ratas.`,
-      false,
-      {mentions: [m.sender]}
-    );
-    await this.updateBlockStatus(m.chat, "block");
-  }
-  return !1;
+let setting = global.db.data.settings[this.user.jid]
+const settingsREAD = global.db.data.settings[this.user.jid] || {}
+
+if (m.text && prefixRegex.test(m.text)) {
+await this.sendPresenceUpdate('composing', m.chat)
+await this.readMessages([m.key])
+        
+let usedPrefix = m.text.match(prefixRegex)[0]
+let command = m.text.slice(usedPrefix.length).trim().split(' ')[0]
+}
+
+if (m.fromMe) return !0
+if (m.isGroup) return !1
+if (!m.message) return !0
+const regexWithPrefix = new RegExp(`^${prefix.source}\\s?${comandos.source}`, 'i')
+if (regexWithPrefix.test(m.text.toLowerCase().trim())) return !0
+
+let chat, user, bot, mensaje
+chat = global.db.data.chats[m.chat]
+user = global.db.data.users[m.sender]
+bot = global.db.data.settings[this.user.jid] || {}
+
+if (bot.antiPrivate && !isOwner && !isROwner) {
+await conn.reply(m.chat, mid.mAdvertencia + mid.smsprivado(m, cuentas), m, { mentions: [m.sender] })  
+await this.updateBlockStatus(m.chat, 'block')
+//await this.updateBlockStatus(m.sender, 'block')
+}
+return !1
 }
