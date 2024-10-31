e31f7d839d3c2296f3502df907ebcf287c09871d
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-10-31 09:22:38 -0500

    Create Play.js

---
+++ plugins/Play.js
@@ -0,0 +1,32 @@
+import yts from 'yt-search';
+
+let handler = async (m, { conn, command, args, text, usedPrefix }) => {
+if (!text) return conn.reply(m.chat, '_Ingresa el nombre de lo que quieres buscar_', m);
+
+await m.react('🕓');
+let res = await yts(text);
+let play = res.videos[0];
+
+if (!play) return conn.reply(m.chat, `No se encontraron resultados`, m)
+
+let { title, thumbnail, ago, timestamp, views, videoId, url } = play;
+
+let txt = '*`ゲ◜៹ YouTube Search ៹◞ゲ`*\n';
+txt += `> Título : *${title || '❌'}*\n`;
+txt += `> Creado : *${ago || '❌'}*\n`;
+txt += `> Duración : *${timestamp || '❌'}*\n`;
+txt += `> Visitas : *${views.toLocaleString() || '❌'}*\n`;
+txt += `> Link : *https://www.youtube.com/watch?v=${videoId}*\n`;
+
+await conn.sendButton(m.chat, txt, author, thumbnail, [
+['Audio', `${usedPrefix}ytmp3 ${url}`],
+['Video', `${usedPrefix}ytmp4 ${url}`]
+], null, [['Hosting', md]], m);
+await m.react('✅')
+}
+
+handler.help = ['play', 'play2']
+handler.tags = ['dl'];
+handler.command = ['playy', 'play2']
+
+export default handler;
