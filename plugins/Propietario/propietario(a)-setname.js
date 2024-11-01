31c4f75f0c71719393294dfc2cfac070f90c9279
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-11-01 13:57:23 -0500

    Update propietario(a)-setname.js

--- plugins/Propietario/propietario(a)-setname.js
+++ plugins/Propietario/propietario(a)-setname.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) return conn.reply(m.chat, `🚩 *Que Nombre Deseas Ponerme?*`, m)
+  if (!text) return conn.reply(m.chat, `🚩 *Que Nombre Deseas Ponerme?*`, m, rcanal)
   try {
     await conn.updateProfileName(text)
-    return conn.reply(m.chat, '✅️ *Nombre Cambiado Con Éxito*', m)
+    return conn.reply(m.chat, '✅️ *Nombre Cambiado Con Éxito*', m, rcanal)
   } catch (e) {
     console.log(e)
     throw `🚩 Ocurrió Un Error¡!`
