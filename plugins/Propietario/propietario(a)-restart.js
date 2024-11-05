42c07b02baf0bed3f112bb23d23283d496d52eec
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-11-05 13:12:48 -0500

    Update propietario(a)-restart.js

--- plugins/Propietario/propietario(a)-restart.js
+++ plugins/Propietario/propietario(a)-restart.js
@@ -1,4 +1,4 @@
-/*import { spawn } from 'child_process'
+import { spawn } from 'child_process'
 let handler = async (m, { conn, isROwner, text }) => {
     if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
     if (conn.user.jid == conn.user.jid) {
@@ -20,4 +20,4 @@ handler.command = ['restart','reiniciar']
 handler.rowner = true
 export default handler
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
-*/
+
