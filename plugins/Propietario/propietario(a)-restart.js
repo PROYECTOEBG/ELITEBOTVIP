7a87aca97e9c1d2aacfaec3bfa9615600bcfa559
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-11-05 10:37:03 -0500

    Update propietario(a)-restart.js

--- plugins/Propietario/propietario(a)-restart.js
+++ plugins/Propietario/propietario(a)-restart.js
@@ -1,4 +1,4 @@
-import { spawn } from 'child_process'
+/*import { spawn } from 'child_process'
 let handler = async (m, { conn, isROwner, text }) => {
     if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
     if (conn.user.jid == conn.user.jid) {
