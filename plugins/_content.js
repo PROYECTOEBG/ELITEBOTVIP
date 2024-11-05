b4029a780189d7bd4709e55cfb5ce9d22a1d08c4
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-11-05 10:11:31 -0500

    Update _content.js

--- plugins/_content.js
+++ plugins/_content.js
@@ -9,7 +9,7 @@ let handler = m => m
 handler.before = async function (m, { conn, isAdmin, isBotAdmin, isOwner, isROwner } ) {
 global.key = ''
 
-global.canales = [canal1, canal2, canal3, canal4].getRandom()
+global.canales = [ canal2, canal3, canal4].getRandom()
 global.welgata = [tk, ig, yt2, yt2, ig, md, ig, yt, paypal, yt2, yt2, ig, fb, tk, ths, asistencia].getRandom()
 global.redesMenu = [canal1, canal2, canal3, canal4, soporteGB, grupo1, grupo2, grupo3, grupo4, grupo5, grupo6, md, ig, paypal, yt, asistencia, fb, tk].getRandom()
 global.accountsgb = [canal1, canal2, canal3, canal4, tk, ig, yt, paypal, fb, ths, md, asistencia].getRandom()
