e676e1f61f8f1bd6450a2f516850d402e1181638
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-11-04 21:33:14 -0500

    Update Ad.js

--- plugins/Ad.js
+++ plugins/Ad.js
@@ -1,55 +1,43 @@
-import path, { join } from 'path'
-import fetch from 'node-fetch';
-import Jimp from 'jimp';
-import fs from 'fs'
-let handler = async (m, { conn, text, command, usedPrefix }) => {
-    let resp, imagen, q
-    if (q == undefined ) {q = m}
-let pp = fs.readFileSync(join(, 'pictures/warn.jpg'))
-let who
-if (m.isGroup) {who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
-} else {who = m.chat}
-let user = global.db.data.bot[conn.user.jid].users[who]
-let bot = global.db.data.bot[conn.user.jid].settings || {}
-if (conn.user.jid == who) { 
-resp = 'No puedo advertirme a mi mismo'
-}
-if (!who)  {
-resp = `*[❗] ETIQUETE A UNA PERSONA O RESPONDA A UN MENSAJE DEL GRUPO PARA ADVERTIR AL USUARIO*\n\n*—◉ EJEMPLO:*\n*${usedPrefix + command} @${global.animxscans[0][0]}*`
-}
-if (user.warn += 1 ) {
-resp = `${user.warn == 1 ? `*@${who.split`@`[0]}*` : `*@${who.split`@`[0]}*`} RECIBIO UNA ADVERTENCIA EN ESTE GRUPO!\n\n*ADVERTENCIAS ${user.warn}/3*\n\n${wm}\n\n📋 LISTA DE ADVERTENCIAS 📋\n => *${usedPrefix}listwarn*`
-imagen = pp
-} 
-if (user.warn >= 3) {
-if (!bot.restrict) {
-    let resp = '*[❗INFO❗] EL PROPIETARIO DEL BOT NO TIENE HABILITADO LAS RESTRICCIONES (#enable restrict) CONTACTE CON EL PARA QUE LO HABILITE*'
-}        
-if (user.warn = 0) {
-resp = `TE LO ADVERTI VARIAS VECES!!\n*@${who.split`@`[0]}* SUPERASTE LAS *3* ADVERTENCIAS, AHORA SERAS ELIMINADO/A 👽`
-user.banned = true
-return conn.groupParticipantsUpdate(m.chat, [who], 'remove') 
-}
-} 
-let txt = '';
-let count = 0;
-for (const c of resp) {
-    await new Promise(resolve => setTimeout(resolve, 5));
-    txt += c;
-    count++;
-    if (count % 10 === 0) {
-      
-    await conn.sendPresenceUpdate('composing' , m.chat);
-    }
-}
-if (resp && imagen) {
-    q = await conn.sendMessage(m.chat, { image: pp, caption: txt.trim(), mentions: conn.parseMention(txt) }, {quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100} )
-} else {
-    return conn.sendMessage(m.chat, { text: txt.trim(), mentions: conn.parseMention(txt) }, {quoted: q, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100} )
-}
-}
-handler.command = /^(advertir|advertencia|warn|warning)$/i
-handler.group = true
-handler.admin = true
-handler.botAdmin = true
-export default handler
+const handler = async (m, { conn, text, command, usedPrefix }) => {
+// if (m.mentionedJid.includes(conn.user.jid)) return; // Evitar advertir al bot mismo
+const pp = 'https://i.imgur.com/vWnsjh8.jpg'
+let number, ownerNumber, aa, who;
+if (m.isGroup) { 
+who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text; 
+} else who = m.chat;
+  const user = global.db.data.users[who];
+  const usuario = conn.user.jid.split`@`[0] + '@s.whatsapp.net'
+  const bot = global.db.data.settings[conn.user.jid] || {};
+  const dReason = 'Sin motivo';
+  const msgtext = text || dReason 
+  const sdms = msgtext.replace(/@\d+-?\d* /g, '');
+  const warntext = `*❌ Etiquete a una persona o responda a un mensaje del grupo para advertir al usuario*\n\n*Ejemplo:*\n*${usedPrefix + command} @tag*`;
+  if (!who) {
+return m.reply(warntext, m.chat, { mentions: conn.parseMention(warntext) });
+  }
+
+for (let i = 0; i < global.owner.length; i++) {
+ownerNumber = global.owner[i][0];
+if (usuario.replace(/@s\.whatsapp\.net$/, '') === ownerNumber) {
+aa = ownerNumber + '@s.whatsapp.net'
+await conn.reply(m.chat, `…`, m, { mentions: [aa] })
+return
+}}
+
+  user.warn += 1;
+  await m.reply(`${user.warn == 1 ? `*@${who.split`@`[0]}*` : `*@${who.split`@`[0]}*`} 𝚁𝙴𝙲𝙸𝙱𝙸𝙾 𝚄𝙽𝙰 𝙰𝙳𝚅𝙴𝚁𝚃𝙴𝙽𝙲𝙸𝙰 𝙴𝙽 𝙴𝚂𝚃𝙴 𝙶𝚁𝚄𝙿𝙾!\nMotivo: ${sdms}\n*Advertencias: ${user.warn}/4*`, null, { mentions: [who] },
+  );
+  if (user.warn >= 4) {
+    user.warn = 0;
+    await m.reply(`𝚃𝙴 𝙻𝙾 𝙰𝙳𝚅𝙴𝚁𝚃𝙸 𝚅𝙰𝚁𝙸𝙰𝚂 𝚅𝙴𝙲𝙴𝚂!!\n*@${who.split`@`[0]}* 𝚂𝚄𝙿𝙴𝚁𝙰𝚂𝚃𝙴 𝙻𝙰𝚂 *4* 𝙰𝙳𝚅𝙴𝚁𝚃𝙴𝙽𝙲𝙸𝙰𝚂, 𝙰𝙷𝙾𝚁𝙰 𝚂𝙴𝚁𝙰𝚂 𝙴𝙻𝙸𝙼𝙸𝙽𝙰𝙳𝙾/𝙰 👽`, null, { mentions: [who] },
+    );
+    await conn.groupParticipantsUpdate(m.chat, [who], 'remove');
+  }
+  return !1;
+};
+
+handler.command = ['advertir', 'advertencia', 'warn', 'warning'];
+handler.group = true;
+handler.admin = true;
+handler.botAdmin = true;
+export default handler;
