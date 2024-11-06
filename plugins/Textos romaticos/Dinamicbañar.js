4d588868c716614b05ada589c0ab122d98f8eb4e
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-11-06 15:25:14 -0500

    Create Dinamicbañar.js

---
+++ plugins/Textos romaticos/Dinamicbañar.js
@@ -0,0 +1,21 @@
+let handler = async (m, { conn, usedPrefix, command}) => {
+let pp = ['https://qu.ax/cZlmq.mp4','https://qu.ax/GrMfw.mp4','https://qu.ax/CUjjB.mp4','https://qu.ax/VXhPe.mp4']
+
+let who
+if (m.isGroup) who = m.mentionedJid[0]
+else who = m.chat
+if (!who) throw '» *Etiqueta al usuario con el que quieres bañarte*\nEjemplo:\n.bañar @kevin'
+let name2 = conn.getName(who)
+let name = conn.getName(m.sender)
+
+await conn.sendMessage(m.chat, { video: { url: pp.getRandom() }, gifPlayback: true, caption: `_*${name}*_` + ' 𝘦𝘴𝘵𝘢 𝘣𝘢𝘯̃𝘢𝘯𝘥𝘰𝘴𝘦 𝘤𝘰𝘯' + ` _*${name2}*_` + ' 🛀🏻🚿\n©𝘌𝘭𝘪𝘵𝘦𝘉𝘰𝘵𝘎𝘭𝘰𝘣𝘢𝘭 -', contextInfo: fakeChannel }, { quoted: m })
+}
+handler.help = ['bañar <@user>']
+handler.tags = ['fun']
+handler.command = ['bañar']
+export default handler
+
+
+
+
+
