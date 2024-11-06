a81739b1bb4bece618618c1668fdec58171ec88b
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-11-06 11:17:59 -0500

    Update ytmp3.js

--- plugins/Descargas/ytmp3.js
+++ plugins/Descargas/ytmp3.js
@@ -1,7 +1,7 @@
-
 import yts from 'yt-search';
 import axios from 'axios';
 import fetch from "node-fetch";
+
 const handler = async (m, { text, usedPrefix, command, conn }) => {
     if (!text) {
         throw m.reply("✧ Ingresa una consulta de *YouTube*");
@@ -11,6 +11,7 @@ const handler = async (m, { text, usedPrefix, command, conn }) => {
     let res = await yts(text);
     let videoList = res.all;
     let videos = videoList[0];
+
     async function ytdl(url) {
         const response = await fetch('https://shinoa.us.kg/api/download/ytdl', {
             method: 'POST',
@@ -23,14 +24,18 @@ const handler = async (m, { text, usedPrefix, command, conn }) => {
                 text: url
             })
         });
+
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
+
         const data = await response.json();
         return data;
     }
+
     let data_play = await ytdl(videos.url);
     console.log(data_play);
+
     if (data_play && data_play.data && data_play.data.mp3) {
         await conn.sendMessage(m.chat, { 
             audio: { url: data_play.data.mp3 }, 
@@ -43,3 +48,10 @@ const handler = async (m, { text, usedPrefix, command, conn }) => {
         await m.react('❌'); 
     }
 };
+
+handler.help = ['ytmp3 <yt url>'];
+handler.tags = ['downloader'];
+handler.command = ['ytmp3', 'yta'];
+handler.register = false;
+
+export default handler;
