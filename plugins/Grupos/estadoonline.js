aa20c9d965e5d230313cbc2401604febab8d032a
ProyectoEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-11-11 08:42:37 -0500

    Update estadoonline.js

--- plugins/Grupos/estadoonline.js
+++ plugins/Grupos/estadoonline.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, isRowner}) => {
 let _muptime
 let totalreg = Object.keys(global.db.data.users).length
 let totalchats = Object.keys(global.db.data.chats).length
-let pp = imagen6
+let pp = imagen7
 if (process.send) {
 process.send('uptime')
 _muptime = await new Promise(resolve => {
