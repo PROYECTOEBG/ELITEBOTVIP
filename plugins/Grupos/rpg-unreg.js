6b0acccebe05dc30c12cd5952458a73ac0c592d9
ProyectoEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-11-14 15:56:44 -0500

    Update rpg-unreg.js

--- plugins/Grupos/rpg-unreg.js
+++ plugins/Grupos/rpg-unreg.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text }) => {
 let user = global.db.data.users[m.sender]
 
 user.registered = false
-return conn.reply(m.chat, `*『✅』 Usted Ya No Está En Mi Base De Datos*\n*By: EliteBotGlobal*`, m,rcanal)
+return conn.reply(m.chat, `*『✅』 Usted Ya No Está En Mi Base De Datos*\n*By: EliteBotGlobal*`, m)
 
 }
 handler.help = ['unreg']
