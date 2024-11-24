f35eb90cc10564f6ee1e9ba01714d5f6159411a0
ProyectoEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-11-23 21:21:34 -0500

    Update Descargassplay.js

--- plugins/Descargas/Descargassplay.js
+++ plugins/Descargas/Descargassplay.js
@@ -33,7 +33,7 @@ renderLargerThumbnail: true
 if (command == 'play') {	
 try {
 await m.react('✅')
-let q = '320kbps'
+let q = '128kbps'
 let v = yt_play[0].url
 const yt = await youtubedl(v).catch(async _ => await youtubedlv2(v))
 const dl_url = await yt.audio[q].download()
