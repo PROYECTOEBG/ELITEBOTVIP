d5d18567acae5b16505394a8f57ee1af53ff9084
ProyectoEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-11-27 21:05:47 -0500

    Update 1.js

--- plugins/1.js
+++ plugins/1.js
@@ -3,18 +3,19 @@ let handler = async(m, { conn }) => {
 //if (!db.data.chats[m.chat].stickers && m.isGroup) throw `${ag}𝙇𝙊𝙎 𝘾𝙊𝙈𝘼𝙉𝘿𝙊𝙎 𝘿𝙀 𝙎𝙏𝙄𝘾𝙆𝙀𝙍𝙎 𝙀𝙎𝙏𝘼𝙉 𝘿𝙀𝙎𝘼𝘾𝙏𝙄𝙑𝘼𝘿𝙊𝙎 𝙐𝙎𝙀 *#on stickers* 𝙋𝘼𝙍𝘼 𝘼𝘾𝙏𝙄𝙑𝘼𝙍\n\n𝙏𝙃𝙀 𝙎𝙏𝙄𝘾𝙆𝙀𝙍𝙎 𝘾𝙊𝙈𝙈𝘼𝙉𝘿𝙎 𝘼𝙍𝙀 𝘿𝙄𝙎𝘼𝘽𝙇𝙀𝘿 𝙐𝙎𝙀 *#on stickers* 𝙏𝙊 𝙀𝙉𝘼𝘽𝙇𝙀`
 if (!db.data.chats[m.chat].stickers && m.isGroup) throw 0
  
-let nombre = 'Adios Basura'
-let nombre2 = '• By: ELiteBot'
+let nombre = '🐈 𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿'
+let nombre2 = '𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨'
  
 const s = [
-'https://file.io/HMiXuNRipiC2'
+'https://i.gifer.com/3OO52.gif'
 ];  
  
 let stiker = await sticker(null, s[Math.floor(Math.random() * s.length)], nombre, nombre2)
 await delay(5 * 5000)
 if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: wm, body: `h`, mediaType: 2, sourceUrl: nn, thumbnail: imagen1}}}, { quoted: m })
 }
-handler.customPrefix = /𝑰𝑵𝑺𝑬𝑹𝑽𝑰𝑩𝑳𝑬|🤡/i 
+handler.customPrefix = /payaso|🤡/i 
 handler.command = new RegExp
+handler.exp = 50
 export default handler
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
